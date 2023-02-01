8441f821fd2edec2760d138ae04ff0b362ae5872
Adam Hammouche <ahammouche@hotmail.com>
2023-02-01 20:11:48 +0100

    feat - defilement infini du caroussel

--- print it main/assets/script.js
+++ print it main/assets/script.js
@@ -1,18 +1,68 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
+	
 ]
+
+const arrowRight = document.querySelector('.arrow_right');
+let img = document.querySelector('.banner-img');
+let counter = 0;
+let tag = document.querySelector('#banner p')
+
+arrowRight.addEventListener('click',function(){
+	// alert('Droite')
+	
+	counter = (counter + 1) % slides.length
+	img.src = slides[counter].image;
+	//let tag = document.querySelector('#banner p')
+	tag.innerHTML = slides[counter].tagLine
+	let bullet = document.querySelectorAll('.dot')
+	for (let i = 0; i < bullet.length; i++) {
+		bullet[i].classList.remove("dot_selected");
+	}
+	
+	const activeIndex = counter
+	bullet[activeIndex].classList.add("dot_selected");
+	
+})
+
+const arrowLeft = document.querySelector('.arrow_left');
+arrowLeft.addEventListener('click',function(){
+	//alert('Gauche')
+	let img = document.querySelector('.banner-img');
+	//counter = (counter - 1 + slides.length) % slides.length;
+	counter = counter - 1
+	if (counter === 0){
+		counter = 4;
+	}
+	img.src = slides[counter].image;
+	let tag = document.querySelector('#banner p')
+	tag.innerHTML = slides[counter].tagLine
+	let bullet = document.querySelectorAll('.dot')
+	for (let i = 0; i < bullet.length; i++) {
+		bullet[i].classList.remove("dot_selected");
+		const activeIndex = counter;
+		bullet[activeIndex].classList.add("dot_selected");
+	}
+	
+	
+})
+
+ 
+
+
